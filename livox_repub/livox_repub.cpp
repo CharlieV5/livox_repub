5dbe5f9dbfeb49338295aaf57efbcd27269b95f4
Charlie <CharlieV@qq.com>
2023-04-16 21:54:34 +0800

    Use pcl::PointXYZI instead of pcl::PointXYZINormal; For 10Hz point frequency, use the intensity to store offset_time and reflectivity.

--- livox_repub/livox_repub.cpp
+++ livox_repub/livox_repub.cpp
@@ -2,31 +2,40 @@
 #include <sensor_msgs/PointCloud2.h>
 #include "livox_ros_driver/CustomMsg.h"
 
-typedef pcl::PointXYZINormal PointType;
+typedef pcl::PointXYZI PointType;
+//typedef pcl::PointXYZINormal PointType;
 typedef pcl::PointCloud<PointType> PointCloudXYZI;
 
 ros::Publisher pub_pcl_out0, pub_pcl_out1;
 uint64_t TO_MERGE_CNT = 1; 
 constexpr bool b_dbg_line = false;
 std::vector<livox_ros_driver::CustomMsgConstPtr> livox_data;
-void LivoxMsgCbk1(const livox_ros_driver::CustomMsgConstPtr& livox_msg_in) {
+
+void LivoxMsgCbk1(const livox_ros_driver::CustomMsgConstPtr& livox_msg_in)
+{
   livox_data.push_back(livox_msg_in);
   if (livox_data.size() < TO_MERGE_CNT) return;
 
   pcl::PointCloud<PointType> pcl_in;
 
-  for (size_t j = 0; j < livox_data.size(); j++) {
+  for (size_t j = 0; j < livox_data.size(); j++)
+  {
     auto& livox_msg = livox_data[j];
     auto time_end = livox_msg->points.back().offset_time;
-    for (unsigned int i = 0; i < livox_msg->point_num; ++i) {
+    for (unsigned int i = 0; i < livox_msg->point_num; ++i)
+    {
       PointType pt;
       pt.x = livox_msg->points[i].x;
       pt.y = livox_msg->points[i].y;
       pt.z = livox_msg->points[i].z;
-      float s = livox_msg->points[i].offset_time / (float)time_end;
+      //float s = livox_msg->points[i].offset_time / (float)time_end;
 
-      pt.intensity = livox_msg->points[i].line +livox_msg->points[i].reflectivity /10000.0 ; // The integer part is line number and the decimal part is timestamp
-      pt.curvature = s*0.1;
+      // The integer part is intensity(0~255) and the decimal part is times offset in second
+      // With 10Hz point frequency, the original offset_time is between 0 and 0.1s
+      pt.intensity = float(livox_msg->points[i].offset_time*1e-9 + livox_msg->points[i].reflectivity);
+
+      //pt.intensity = float(livox_msg->points[i].reflectivity);
+
       pcl_in.push_back(pt);
     }
   }
@@ -38,20 +47,23 @@ void LivoxMsgCbk1(const livox_ros_driver::CustomMsgConstPtr& livox_msg_in) {
   sensor_msgs::PointCloud2 pcl_ros_msg;
   pcl::toROSMsg(pcl_in, pcl_ros_msg);
   pcl_ros_msg.header.stamp.fromNSec(timebase_ns);
-  pcl_ros_msg.header.frame_id = "/livox";
+  pcl_ros_msg.header.frame_id = "/livox_frame";
   pub_pcl_out1.publish(pcl_ros_msg);
   livox_data.clear();
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
   ros::init(argc, argv, "livox_repub");
   ros::NodeHandle nh;
 
   ROS_INFO("start livox_repub");
 
   ros::Subscriber sub_livox_msg1 = nh.subscribe<livox_ros_driver::CustomMsg>(
-      "/livox/lidar", 100, LivoxMsgCbk1);
-  pub_pcl_out1 = nh.advertise<sensor_msgs::PointCloud2>("/livox_pcl0", 100);
+      "/livox/lidar", 10, LivoxMsgCbk1);
+  pub_pcl_out1 = nh.advertise<sensor_msgs::PointCloud2>("/livox_pc2", 10);
 
   ros::spin();
 }
+
+
